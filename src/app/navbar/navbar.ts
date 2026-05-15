62b4328629761eebd6cc1780a666d9dcb29f605e
Prateek Jain <pattrick5.pj@gmail.com>
2026-05-15 23:08:27 +0530

    updated home page and navbar

--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -8,5 +8,12 @@ import { AuthService } from '../services/auth.service';
   styleUrl: './navbar.css',
 })
 export class Navbar {
+  menuOpen = false;
+  citiesOpen = false;
+
   constructor(public auth: AuthService) {}
+
+  toggleMenu()  { this.menuOpen = !this.menuOpen; }
+  toggleCities(e: Event) { e.preventDefault(); this.citiesOpen = !this.citiesOpen; }
+  closeMenu()   { this.menuOpen = false; this.citiesOpen = false; (document.activeElement as HTMLElement)?.blur(); }
 }
